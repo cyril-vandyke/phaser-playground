3ffe4f428489dcde21315a8bae9c8159a14d6fa5
Cyril Van Dyke <cvandyke@navexglobal.com>
2018-01-10 14:14:37 -0800

    Add player2 and text

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -1,9 +1,9 @@
 class Player extends Phaser.Sprite {
 
-    constructor(game, position) {
-        super(game, position.x, position.y, 'spritesheet', 'alienPink_stand.png');
+    constructor(game, position, color) {
+        super(game, position.x, position.y, 'spritesheet', color + '_stand.png');
         
-
+        this.playerColor = color;
         this.game.physics.arcade.enable(this);
         this.body.gravity.y = 400;
         this.body.collideWorldBounds = true;
@@ -12,7 +12,7 @@ class Player extends Phaser.Sprite {
         this.scale.setTo(0.5);
         this.anchor.setTo(0.5);
 
-        this.animations.add('walk', ['alienPink_walk1.png','alienPink_walk2.png'], 6, true);
+        this.animations.add('walk', [ this.playerColor + '_walk1.png', this.playerColor + '_walk2.png'], 6, true);
         
         this.game.stage.addChild(this);
     }
@@ -34,7 +34,7 @@ class Player extends Phaser.Sprite {
 		else
 		{
 			this.animations.stop();
-			this.frameName = 'alienPink_stand.png';
+			this.frameName = this.playerColor + '_stand.png';
 		}
 	
 		if (cursors.up.isDown && this.body.touching.down && playerOnGround)
@@ -44,7 +44,7 @@ class Player extends Phaser.Sprite {
 
 		if (this.body.velocity.y !== 0) {
 			this.animations.stop();
-			this.frameName = 'alienPink_jump.png';
+            this.frameName = this.playerColor + '_jump.png';
 		}
     }
 }
